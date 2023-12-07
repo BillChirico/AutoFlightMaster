f5b1cd0b61e78c8e00fe64b238dff4e093a60945
Bill Chirico <bill@chirico.dev>
2023-12-07 16:51:35 -0500

    Update and refactor

--- AutoFlightMaster.lua
+++ AutoFlightMaster.lua
@@ -1,16 +1,32 @@
---------------------------------------
+---------------------------------------------
 -- Canel Auras & Dismount at Flight Master --
---------------------
+---------------------------------------------
 -- Author: Bapes
---------------------------------------
+---------------------------------------------
 
---- Event frame for opening and closing the taxi map.
+local spell = Spell:CreateFromSpellID(403789)
+local spellName
+
+-- -- Get Metamorphosis spell name
+spell:ContinueOnSpellLoad(function()
+    spellName = spell:GetSpellName()
+end)
+
 CreateFrame("Frame", "TaxiOpenEventFrame", UIParent)
 TaxiOpenEventFrame:RegisterEvent("TAXIMAP_OPENED")
+TaxiOpenEventFrame:RegisterEvent("ADDON_LOADED")
 TaxiOpenEventFrame:SetScript("OnEvent", function(self, event, ...)
 
+	if (event == "ADDON_LOADED") then
+		local addonName = ...
+		if (addonName == "AutoFlightMaster") then
+			print("|cff00CCFF AutoFlightMaster loaded by Bapes! If you have any questions, comments, or suggestions, please don't hesitate to connect with me on Discord (Bapes) or CurseForge. Enjoy your journey with AutoFlightMaster! |r")
+		end
+	end
+
 	if ( event == "TAXIMAP_OPENED" ) then
-		CancelSpellByName('Metamorphosis')
+		print(spellName)
+		CancelSpellByName(spellName)
 
 		if ( IsMounted() ) then
 			Dismount();
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AutoFlightMaster
-**Experience seamless interactions with flight masters in-game! With this addon,** you won't have to endure the frustration of being yelled at when selecting a flight path. It automatically cancels specific auras like Metamorphosis and ensures you're dismounted for a smooth interaction with the flight master.
+**Experience seamless interactions with flight masters in-game?!**
+With this addon, you won't have to endure the frustration of being yelled at when selecting a flight path.
+It automatically cancels specific auras like Metamorphosis and ensures you're dismounted for a smooth interaction with the flight master.
+
+# Contact
+You can contact me on Discord at **Bapes**!
